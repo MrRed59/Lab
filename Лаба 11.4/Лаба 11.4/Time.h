1b4b89173acbae6a367bf8d1987c6572cd22210e
Alex Erypalov <erypaliv1234@gmail.com>
2022-04-25 20:46:49 +0500

    fin

---
+++ Лаба 11.4/Лаба 11.4/Time.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include<iostream>
+
+
+class Time
+{
+	int m_min;
+	int m_sec;
+public:
+	//---------constructor-----------
+	Time();
+	Time(int, int);
+	Time(const Time&);
+	~Time();	
+	//-------Getters and Setters----
+	int get_min();
+	int get_sec();
+	void set_min(int);
+	void set_sec(int);
+	//-------overloaded-functions----
+	Time& operator=(const Time&);
+	Time operator+(const Time&);
+	Time& operator+=(const Time&);
+	Time operator/(const Time&);
+	Time& operator/=(const Time&);
+	Time operator/(int);
+	bool operator>(const Time&);
+	bool operator<(const Time&);
+	bool operator==(const Time&);
+	bool operator!=(const Time&);
+	//-------friend-functions---------
+	friend std::ostream& operator<<(std::ostream&, const Time&);
+	friend std::istream& operator>>(std::istream&, Time&);
+};
+
