663d8ce285ed09b04f774d37b57e7f5e39b4dd0c
Alex Erypalov <erypaliv1234@gmail.com>
2022-04-26 21:23:31 +0500

    fin

--- Лаба 11.1 fin/Лаба 11.1 fin/main.cpp
+++ Лаба 11.1 fin/Лаба 11.1 fin/main.cpp
@@ -8,7 +8,10 @@ Vec make_vector(int n) //функция для формирования вект
     Vec v; //пустой вектор
     for (int i = 0; i < n; i++)
     {
-        float a = rand() % 100 - 50;
+        //srand(unsigned(time(0)));
+
+        float a = ((float)rand() / RAND_MAX) + rand() % 100 - 50;
+
         v.push_back(a); //добавить а в конец вектора
     }
     return v;
@@ -19,14 +22,14 @@ void print_vector(const Vec& v)
     for (size_t i = 0; i < v.size(); i++)
     {
         if (i)
-            std::cout << ';' << v[i];
+            std::cout << "; \t" << v[i];
         else
             std::cout << v[i];
     }
     std::cout << "." << std::endl;
 }
 
-int arithmetic_mean(const Vec& v)
+float arithmetic_mean(const Vec& v)
 {
     float s = 0;
 
@@ -38,58 +41,69 @@ int arithmetic_mean(const Vec& v)
     return s / n;
 }
 
-void add_vector(Vec& v, int el, int pos)
+void add_vector(Vec& v, float el, int pos)
 {
     v.insert(v.begin() + pos, el); //добавить на место pos элемент el
 }
 
-int max(const Vec& v)
-{
-    int m = v[0]; //мин. элемент
-    int n = 0; //номер мик. элемента
-    for (size_t i = 0; i < v.size(); i++)
-    {
-        if (m < v[i])
-        {
-            m = v[i]; //макс. элемент
-            n = i; //номер макс. элемента
-        }
-    }
-    return n;
-}
+//int max(const Vec& v)
+//{
+//    int m = v[0]; //мин. элемент
+//    int n = 0; //номер мин. элемента
+//    for (size_t i = 0; i < v.size(); i++)
+//    {
+//        if (m < v[i])
+//        {
+//            m = v[i]; //макс. элемент
+//            n = i; //номер макс. элемента
+//        }
+//    }
+//    return n;
+//}
 
-void del_vector(Vec& v, int pos) //удалить элемент из позиции pos
+void remove_element_by_index(Vec& v, int pos) //удалить элемент из позиции pos
 {
     v.erase(v.begin() + pos);
 }
 
 int min(const Vec& v) //поиск минимального элемента
 {
-    int m = v[0]; //мин. элемент
-    int n = 0; //номер мик. элемента
+    float m = v[0]; //мин. элемент
+    int n = 0; //номер мин. элемента
     for (size_t i = 0; i < v.size(); i++)
     {
         if (m > v[i])
         {
             m = v[i]; //мин. элемент
-            n = i; //номер мик. элемента
+            n = i; //номер мин. элемента
         }
     }
     return n;
 }
 
-void division(Vec& v)
+void subtract_min_element(Vec& v)
 {
     int m = min(v);
+    float min_element = v[m];
     for (size_t i = 0; i < v.size(); i++)
     {
-        v[i] /= v[m];
+        v[i] -= min_element;
     }
 }
+
+void add_to_the_end_of_the_vector(Vec& v)
+{
+    int m = min(v);
+    v.push_back(v[m]);
+}
+
+
 
 
 int main()
 {
+    srand(unsigned(time(0)));
+
     try
     {
         std::vector<float> v;
@@ -100,8 +114,8 @@ int main()
         v = make_vector(n); //формирование вектора
         print_vector(v); //печать вектора
 
-        int el = arithmetic_mean(v); //вычисление средне - арифметического значения
-        std::cout << "Position? ";
+        float el = arithmetic_mean(v); //вычисление средне - арифметического значения
+        std::cout << "\nPosition for adding an element? ";
         int pos;
         std::cin >> pos;
 
@@ -109,20 +123,22 @@ int main()
             throw 1;
 
         add_vector(v, el, pos); //вызов функции для добавления
-        std::cout << "add_vector: " << std::endl;
+        std::cout << "\nadd_vector: " << std::endl;
         print_vector(v); //печать вектора
 
         //
 
-        int n_max = max(v); //поиск номера максимального элемента
-        del_vector(v, n_max); //удалить элемент с этим номером
-        std::cout << "del_vector: " << std::endl;
+        std::cout << "\nPosition to delete an element? ";
+        std::cin >> pos;
+
+        remove_element_by_index(v, pos); //удалить элемент с этим номером
+        std::cout << "\nRemove element by index: " << std::endl;
         print_vector(v);
 
         //
 
-        division(v); //каждый элемент разделить на мин. значение вектора
-        std::cout << "division: " << std::endl;
+        subtract_min_element(v); //каждый элемент разделить на мин. значение вектора
+        std::cout << "\nSubtract min element: " << std::endl;
         print_vector(v);
     }
     catch (int)
