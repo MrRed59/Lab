36583710e73bd56ba469c797f1b878ef98f5d835
Alex Erypalov <erypaliv1234@gmail.com>
2022-05-04 17:49:20 +0500

    fin

--- Лаба 11.3 fin/Лаба 11.3 fin/Vector.h
+++ Лаба 11.3 fin/Лаба 11.3 fin/Vector.h
@@ -1,44 +1,57 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include<ctime>
 
 
 template <class T>
 class Vector
 {
 	std::vector <T> v;
-	int len;
+	//int len;
 public:
 	//---------constructor-----------
-	Vector();
+	//Vector() :len(0) {};
 	Vector(int);
 	~Vector();
 	//-------overloaded-functions----
+	void Add();
 	void Print();
-
+	size_t Size();
+	Vector& operator=(const Vector <T>&);
+	Vector operator+(const T&);
+	Vector operator*(const Vector <T>&);
+	T& operator[](int);
 };
 
-template<class T>
-inline Vector<T>::Vector()
-{
-	len = 0;
-}
+//template<class T>
+//inline Vector<T>::Vector()
+//{
+//	size = 0;
+//}
 
 template<class T>
 inline Vector<T>::Vector(int n)
 {
-	T a;
+	T a{};
 	for (int i = 0; i < n; i++)
-	{
-		std::cin >> a;
 		v.push_back(a);
-	}
-	len = v.size();
 }
 
 template<class T>
 inline Vector<T>::~Vector()
 {
+}
+
+template<class T>
+inline void Vector<T>::Add()
+{
+	T a{};
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		std::cin >> a;
+		v[i] = a;
+	}
 }
 
 template<class T>
@@ -52,4 +65,114 @@ inline void Vector<T>::Print()
 			std::cout << v[i];
 	}
 	std::cout << "." << std::endl;
+}
+
+template<class T>
+inline size_t Vector<T>::Size()
+{
+	return v.size();
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& temp)
+{
+	if (this == &temp)
+		return *this;
+	v.clear();
+
+	for (size_t i = 0; i < temp.v.size(); i++)
+	{
+		v.push_back(temp.v[i]);
+	}
+	return *this;
+}
+
+template <class T>
+Vector<T> Vector<T>::operator+(const T& k)
+{
+	Vector<T> temp(v.size());
+	for (size_t i = 0; i < v.size(); i++)
+		temp.v[i] = v[i] + k;
+	return temp;
+}
+
+template <class T>
+Vector<T> Vector<T>::operator*(const Vector<T>& second)
+{
+	Vector<T> temp(v.size());
+
+	size_t n = 0;
+
+	//поиск пересечения множества и запись в промежуточный класс
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		for (size_t j = 0; j < second.v.size(); j++)
+		{
+			if (v[i] == second.v[j])
+			{
+				temp.v[n] = v[i];
+				n++;
+				break;
+			}
+		}
+	}
+
+	//если нет пересечений
+	if (!n)
+	{
+		Vector<T> null(0);
+		std::cout << "No intersections found" << std::endl;
+		return null;
+	}
+
+	//сортировка по возрастанию
+	T value = 0;
+	for (size_t i = 0; (i < (n - 1)) && (i < (v.size() - 1)); i++)
+	{
+		for (size_t j = i + 1; (j < n) && (j < v.size()); j++)
+		{
+			if (temp.v[i] >= temp.v[j])
+			{
+				value = temp.v[i];
+				temp.v[i] = temp.v[j];
+				temp.v[j] = value;
+			}
+		}
+	}
+
+	Vector<T> temp_short(temp, n);
+
+	size_t counter = 1;
+
+	//запись в класс temp_short чисел без повторения
+	for (size_t i = 0; (i < (n - 1)) && n && (i < v.size()); i++)
+	{
+		//if (temp.v[i] != temp.v[i + 1])
+		//{
+		//	temp_short.v[i] = temp.v[i];
+		//	counter++;
+		//}
+		if (temp.v[i] == temp.v[i + 1])
+			v.erase(i + 1);
+	}
+
+	return temp;
+
+	//if (temp.v.size() == counter)
+	//	return temp;
+	//else
+	//{
+	//	Vector<T> temp_fin(temp, counter);
+	//	return temp_fin;
+	//}
+}
+
+template <class T>
+T& Vector<T>::operator[](int index)
+{
+	if (index < v.size())
+		return v[index];
+	else
+		std::cout << "Error. Index > Size" << std::endl;
+	exit(1);
 }
--- Лаба 11.3 fin/Лаба 11.3 fin/main.cpp
+++ Лаба 11.3 fin/Лаба 11.3 fin/main.cpp
@@ -1,10 +1,53 @@
 #include<iostream>
 #include"Vector.h"
-#include"Time.h"
+#include"Money.h"
 
 
 int main()
 {
-	Vector<Time>vec(5);
-	vec.Print();
+	Vector<Money>vec(2);
+
+	std::cout << "Money \'1\': ";
+	std::cin >> vec[0];
+
+	int k;
+
+	std::cout << "k: ";
+	std::cin >> k;
+
+	std::cout << "Money \'2\': ";
+	std::cin >> vec[1];
+
+	vec[1] = vec[1] + k;
+	std::cout << "Money \'2\'+ k : " << vec[1] << std::endl;
+
+	std::cout << "Enter the elements in Money A: ";
+	Vector <Money> V_money_1(5);
+	V_money_1.Add();
+	std::cout << "Money A: ";
+	V_money_1.Print();
+	std::cout << std::endl;
+
+	std::cout << "Money B: " << std::endl;
+	Vector <Money> V_money_2(10);
+	V_money_2.Add();
+	std::cout << "Money B: " << std::endl;
+	V_money_2.Print();
+	std::cout << std::endl;
+
+	V_money_2 = V_money_1;
+
+	std::cout << "B = A: \t";
+	V_money_2.Print();
+	std::cout << std::endl;
+
+
+	std::cout << "A[2]: \t" << V_money_1[2] << std::endl;
+	std::cout << "Size \'A\': \t" << V_money_2.Size() << std::endl;
+
+	V_money_2 = V_money_1 + vec[0];
+
+	std::cout << "B = A + Money 1: \t";
+	V_money_2.Print();
+	std::cout << std::endl;
 }
