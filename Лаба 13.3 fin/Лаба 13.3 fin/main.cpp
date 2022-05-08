5fca1782dc385892cd5fec60b31f8c6cb9fbf3ba
Alex Erypalov <erypaliv1234@gmail.com>
2022-05-08 18:50:59 +0500

    fin?

--- Лаба 13.3 fin/Лаба 13.3 fin/main.cpp
+++ Лаба 13.3 fin/Лаба 13.3 fin/main.cpp
@@ -1,68 +1,81 @@
 ﻿#include<iostream>
 #include<vector>
+#include<map>
 #include<algorithm>
 
 #include"Money.h"
 
-typedef std::vector<Money> TMoney;
+typedef std::map<int, Money> TMap;
+typedef TMap::iterator it;
 Money temp;
+
 
 struct Greater_temp
 {
-	bool operator()(Money& m_temp)
+	bool operator()(const std::pair<int, Money>& m_temp)
 	{
-		return (m_temp > temp);
+		return (m_temp.second > temp);
 	}
 };
 
 struct Comp_less
 {
-	bool operator()(const Money& _Left, const Money& _Right)
+	bool operator()(const std::pair<int, Money>& _Left, const std::pair<int, Money>& _Right)
 	const{
-		return (_Left > _Right);
+		return (_Left.second > _Right.second);
 	}
 };
 
 struct Equal_temp
 {
-	bool operator()(Money& m_temp)
-	const{
-		return(m_temp == temp);
+	bool operator()(std::pair<int, Money> m_temp)
+	{
+		return(m_temp.second == temp);
 	}
 };
 
-TMoney make_vector(const size_t n)
+TMap make_map(const size_t n)
 {
 	Money money;
-	TMoney tmoney;
+	TMap tmoney;
 	for (size_t i = 0; i < n; i++)
 	{
 		std::cin >> money;
-		tmoney.push_back(money);
+		tmoney.insert(std::make_pair(i, money));
 	}
 	return tmoney;
 }
 
-void print_vector(const TMoney& tmoney)
+void print_map(TMap m)
 {
-	for (auto &i: tmoney)
-	{
-		std::cout << i << ": ";
-	}
-	std::cout << std::endl;
+	for (size_t i = 0; i < m.size(); i++)
+		std::cout << i << " : " << m[i] << " " << std::endl;
 }
 
-Money arithmetic_mean(const TMoney& tmoney)
+Money arithmetic_mean(TMap v)
 {
-	Money temp = tmoney[0];
-	for (size_t i = 0; i < tmoney.size(); i++)
-		temp += tmoney[i];
-	return temp / tmoney.size();
+	Money s{};
+	for (size_t i = 0; i < v.size(); i++)
+		s += v[i];
+	return s / v.size();
 }
 
-void division(Money& money)
+void division(TMap& v)
 {
-	money /= temp;
+	it i = std::max_element(v.begin(), v.end());
+	Money m = i->second;
+	for (size_t i = 0; i < v.size(); i++)
+		v[i] = v[i] / m;
+}
+
+void add_sum_max_min(TMap& s)
+{
+	it i = std::max_element(s.begin(), s.end());
+	Money sum_min_max = i->second;
+	i = std::max_element(s.begin(), s.end());
+	sum_min_max += i->second;
+	for (size_t i = 0; i < s.size(); i++)
+		s[i] += sum_min_max;
 }
 
 
@@ -71,48 +84,45 @@ int main()
 	size_t n;
 	std::cout << "N? ";
 	std::cin >> n;
-	TMoney tmoney;
-	tmoney = make_vector(n);
-	print_vector(tmoney);
+	TMap tmoney;
+	tmoney = make_map(n);
+	print_map(tmoney);
 
-	TMoney::iterator i;
+	it i;
 	i = std::max_element(tmoney.begin(), tmoney.end());
-	std::cout << "Max: " << *i << std::endl;
-	Money money_max = *i;
+	std::cout << "Max: " << i->second << std::endl;
+	Money money_max = i->second;
 	temp = arithmetic_mean(tmoney);
 	std::cout << "Arithmetic mean: " << temp << std::endl;
-	std::replace_if(tmoney.begin(), tmoney.end(), Greater_temp(), money_max);
-	std::cout << "Replacement: " << std::endl;
-	print_vector(tmoney);
 
-	std::cout << "Descending sort: " << std::endl;
-	std::sort(tmoney.begin(), tmoney.end(), Comp_less());
-	print_vector(tmoney);
-
-	std::cout << "Ascending sort: " << std::endl;
-	std::sort(tmoney.begin(), tmoney.end());
-	print_vector(tmoney);
 
 	std::cout << "Search: " << std::endl;
 	std::cin >> temp;
 	i = std::find_if(tmoney.begin(), tmoney.end(), Equal_temp());
 	if (i != tmoney.end())
-		std::cout << *i << std::endl;
+		std::cout << i->second << std::endl;
 	else
 		std::cout << "Not such element!" << std::endl;
 
-	std::cout << "Deletion: " << std::endl;
+	std::cout << "Removing the minimum element: " << std::endl;
 	i = std::min_element(tmoney.begin(), tmoney.end());
-	temp = *i;
-	i = std::remove_if(tmoney.begin(), tmoney.end(), Equal_temp());
-	tmoney.erase(i, tmoney.end());
-	print_vector(tmoney);
+	temp = i->second;
+	i = tmoney.find(i->first);
+	tmoney.erase(i);
+	print_map(tmoney);
 
 	std::cout << "Division: " << std::endl;
-	i = std::max_element(tmoney.begin(), tmoney.end());
-	temp = *i;
-	std::for_each(tmoney.begin(), tmoney.end(), division);
-	print_vector(tmoney);
+	division(tmoney);
+	print_map(tmoney);
+
+	std::cout << "Add a minimal element: " << std::endl;
+	i = std::min_element(tmoney.begin(), tmoney.end());
+	tmoney.insert(std::make_pair(i->first, i->second));
+	print_map(tmoney);
+
+	std::cout << "Add the sum of the maximum and minimum: " << std::endl;
+	add_sum_max_min(tmoney);
+	print_map(tmoney);
 
 	return 0;
 }
